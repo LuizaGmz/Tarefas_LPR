0863e53f4948c2d4cc426bbcd33150260dc8a83d
LuizaGmz <luizalimagomes04@gamil.com>
2025-03-23 20:51:16 -0300

    commit

---
+++ num_par_imp/num_par_impar/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Vamos ler se um numero é impar ou par. Insira um numero");
+num = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (num%2 == 0){
+    Console.WriteLine($"O numero {num} é par. LEGAL  ");
+}
+
+else {
+    Console.WriteLine($"Eita, o numero {num} não é par, que tritse");
+}
+
---
+++ rpg/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int num, escolha, cor;
+
+
+
+Console.WriteLine("Vamos contruir seu personagem!!");
+Console.WriteLine("Escolha uma classe ");
+Console.WriteLine("1 - Guerreiro ");
+Console.WriteLine("2 - Mago  ");
+Console.WriteLine("3 - Arqueiro ");
+escolha = Convert.ToInt32(Console.ReadLine());
+
+switch(escolha){
+    case 1:
+
+        Console.WriteLine(" Você escolheu a classe Guerreiro. Eis aqui suas habilidades: ");
+        Console.WriteLine("Ataque Pesado");
+        Console.WriteLine("Defesa total"); 
+        Console.WriteLine("Retaliação com a espada");
+        Console.WriteLine("ULT SUPREMA: Golpe do Dragão");
+        
+        break;
+
+    case 2:
+
+        Console.WriteLine(" Você escolheu a classe Mago. Eis aqui suas habilidades: ");
+        Console.WriteLine("Bola de Fogo");
+        Console.WriteLine("Escudo de Gelo"); 
+        Console.WriteLine("Redemoinho de vento");
+        Console.WriteLine("ULT SUPREMA: Tempestade de Raios");
+
+        break;
+
+    case 3:
+
+        Console.WriteLine(" Você escolheu a classe Arqueiro. Eis aqui suas habilidades: ");
+        Console.WriteLine("Flecha Precisa");
+        Console.WriteLine("Disparo Triplo"); 
+        Console.WriteLine("Tempestade de Flechas");
+        Console.WriteLine("ULT SUPREMA: Olho Morto");
+
+        break;
+
+        case 4:
+
+        Console.WriteLine(" Você desbloqueou uma classe secreta");
+        Console.WriteLine("Deseja prosseguir?");
+        Console.WriteLine("1 - Sim");
+        Console.WriteLine("2 - Não");
+
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if(num == 1){
+            
+            Console.WriteLine("Você desbloqueo a classe JEDI!!! ");
+            Console.WriteLine("Eis aqui suas habilidades: ");
+            Console.WriteLine("Refletir Tiros");
+            Console.WriteLine("Força");
+            Console.WriteLine("Estilo Shii-Cho");
+            Console.WriteLine("ULT SUPREMA: Despertar da Força");
+
+           
+            Console.WriteLine("Voce adquiriu o item: Sabre de Luz");
+            Console.WriteLine("Escolha uma cor para seu sabre de luz: ");
+            Console.WriteLine("1 - Azul");
+            Console.WriteLine("2 - Vermelho");
+
+            cor = Convert.ToInt32(Console.ReadLine());
+
+            if(cor == 1){
+                Console.WriteLine("Seu sabre de luz é azul");
+                Console.WriteLine("Que a força esteja com você");
+            }
+
+            else {
+                Console.WriteLine("Seu sabre de luz é vermelho");
+                Console.WriteLine("Que a força esteja com você");
+            }
+                     
+              
+        }
+
+        else {
+            Console.WriteLine("Você escolheu não prosseguir...volte a tela inicial e escolha uma classe");
+        }
+        break;
+}
+
+
