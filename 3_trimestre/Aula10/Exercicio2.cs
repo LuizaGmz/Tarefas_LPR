7a474c7375a102662b37c24602200bfc23f06231
LuizaGmz <luizalimagomes04@gmail.com>
2025-11-03 07:25:24 -0300

    two thousents years later

---
+++ 3_trimestre/Aula10/Exercicio2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Funcionario
+{
+    public string nome;
+    public double salarioBrut;
+    public double salarioLiq;
+    public double imposto;
+
+    public double calcImposto()
+    {
+        if (salarioBrut < 2000)
+        {
+            return salarioBrut * 0.1;
+        }
+        else if (salarioBrut <= 3000)
+        {
+            return salarioBrut * 0.15;
+        }
+        else
+        {
+            return salarioBrut * 0.2;
+        }
+
+    }
+
+    public double calcSalarioLiq()
+    {
+        imposto = calcImposto();
+        return salarioBrut - imposto;
+    }
+
+    public void exibFunc()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Salário Bruto: {salarioBrut}");
+        Console.WriteLine($"Imposto: {imposto}");
+        Console.WriteLine($"Salário Líquido: {salarioLiq}");
+    }
+    
+    public void aumentarSalario(double perc)
+    {
+        salarioBrut += salarioBrut * (perc / 100);
+    }
+
+  
+
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Funcionario func = new Funcionario();
+
+        Console.WriteLine("Digite o nome do funcionário:");
+        func.nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o salário bruto do funcionário:");
+        func.salarioBrut = Convert.ToDouble(Console.ReadLine());
+
+        func.salarioLiq = func.calcSalarioLiq();
+        func.exibFunc();
+
+        Console.WriteLine("Digite a porcentagem de aumento salarial:");
+        double perc = Convert.ToDouble(Console.ReadLine());
+        func.aumentarSalario(perc);
+
+        func.salarioLiq = func.calcSalarioLiq();
+        Console.WriteLine("Após o aumento salarial:");
+        func.exibFunc();
+    }
+}
---
+++ 3_trimestre/Aula10/Exercicio3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Aluno
+{
+    public string nome;
+    public string matricula;
+    public double notaProv1;
+    public double notaProv2;
+    public double notaTrab;
+
+    public double CalcularMedia()
+    {
+       return (notaProv1 * 2.5 + notaProv2 * 2.5 + notaTrab * 2) / 7;
+    }
+    public double verifyFinal()
+    {
+        double media = CalcularMedia();
+        if (media >= 7)
+        {
+            return 0; // Aprovado
+        }
+        else
+        {
+            return 10 - media; // Nota necessária para passar
+        }
+    }
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno();
+
+        Console.Write("Digite o nome do aluno: ");
+        aluno.nome = Console.ReadLine();
+
+        Console.Write("Digite a matrícula do aluno: ");
+        aluno.matricula = Console.ReadLine();
+
+        Console.Write("Digite a nota da primeira prova: ");
+        aluno.notaProv1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a nota da segunda prova: ");
+        aluno.notaProv2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a nota do trabalho: ");
+        aluno.notaTrab = Convert.ToDouble(Console.ReadLine());
+
+        double media = aluno.CalcularMedia();
+        Console.WriteLine($"Média do aluno {aluno.nome}: {media}");
+
+        double notaFinal = aluno.verifyFinal();
+        if (notaFinal == 0)
+        {
+            Console.WriteLine("Aluno aprovado!");
+        }
+        else
+        {
+            Console.WriteLine($"Aluno reprovado! Nota necessária para passar: {notaFinal}");
+        }
+    }
+}
